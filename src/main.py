15bed0e758b9f3fd3841e7c0fd422cc492a462e1
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-02 11:46:26 -0500

    Update assertions and output for cel_to_fah
    Refactor temperature conversion assertions and output.

--- src/main.py
+++ src/main.py
@@ -4,16 +4,24 @@ from temp_func import cel_to_fah, fah_to_cel, temp_to_kel
 deg_str = chr(176)
 # - cel_to_fah
 # TODO - (Input) Assign data for test cases
-arg = f"{0}{ deg_str}C"
+arg = f"{0 }{deg_str}C"
 exp = f"{32}{deg_str}F"
 act = cel_to_fah(arg)
-assert act == exp, f"{act} is not {exp} ? review src/cel_to_fah"
+
 # TODO - (Process) Introduce the assert statememts
+assert act == exp, f"{act} is not {exp} ? review cel_to_fah"
+
+# TODO - (Output) Print passing cases
+opt = "cel_to_fah:\n\t"
+opt += f"{arg}{deg_str}C is {exp}{deg_str}F"
+print(opt)
 
 # - fah_to_cel
 # TODO - (Input) Assign data for test cases
 # TODO - (Process) Introduce the assert statememts
+# TODO - (Output) Print passing cases
 
 # - temp_to_kel
 # TODO - (Input) Assign data for test cases
 # TODO - (Process) Introduce the assert statememts
+# TODO - (Output) Print passing cases
