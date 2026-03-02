ace718d003d3bc5c860b00003c04a415f5ee4a09
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-02 17:55:40 -0500

    Refactor test case setup for cel_to_fah function

--- src/main.py
+++ src/main.py
@@ -1,28 +1,24 @@
-from temp_func import cel_to_fah, fah_to_cel, temp_to_kel
+from temp_func import deg_str, print_steps, cel_to_fah, fah_to_cel, temp_to_kel
 # DO NOT UPDATE CODE ABOVE THIS LINE
 
-deg_str = chr(176)
 # - cel_to_fah
-# TODO - (Input) Assign data for test cases
-arg = f"{0 }{deg_str}C"
-exp = f"{32}{deg_str}F"
-act = cel_to_fah(arg)
+# TODO - (Input) Assign data for test cases - step 1
+arg, exp, act = f"{0 }{deg_str}C", f"{32}{deg_str}F", cel_to_fah(arg)
 
-# TODO - (Process) Introduce the assert statememts
-assert act == exp, f"{act} is not {exp} ? review cel_to_fah"
+# TODO - (Process) Introduce the assert statememts - step 3
+assert act == exp, f"{print_steps(arg)} {act} != {exp} ? review temp_func.py"
 
-# TODO - (Output) Print passing cases
+# TODO - (Output) Print passing cases - step 4
 opt = "cel_to_fah:\n\t"
 opt += f"{arg}{deg_str}C is {exp}{deg_str}F"
 print(opt)
 
-assert False, 'test'
 # - fah_to_cel
-# TODO - (Input) Assign data for test cases
-# TODO - (Process) Introduce the assert statememts
-# TODO - (Output) Print passing cases
+# TODO - (Input) Assign data for test cases - step 1
+# TODO - (Process) Introduce the assert statememts - step 3
+# TODO - (Output) Print passing cases - step 4
 
 # - temp_to_kel
-# TODO - (Input) Assign data for test cases
-# TODO - (Process) Introduce the assert statememts
-# TODO - (Output) Print passing cases
+# TODO - (Input) Assign data for test cases - step 1
+# TODO - (Process) Introduce the assert statememts - step 3
+# TODO - (Output) Print passing cases - step 4
