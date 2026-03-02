7e2e814b5cb89ae785840a0260e59add220d3b1f
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-02 15:01:10 -0500

    Update temp_func.py

--- src/temp_func.py
+++ src/temp_func.py
@@ -2,15 +2,15 @@ deg_str = chr(176)
 
 def cel_to_fah(temp_c):
   temp_lst = temp_c.split(deg_str)
-  f_val = float(temp_lst[0])
-  cels  = f_val * 9/5 + 32 
-  return f"{cels:.1f}{deg_str}F"
+  c_val = float(temp_lst[0])
+  fah  = c_val * 9/5 + 32 
+  return f"{fah:.1f}{deg_str}F"
 
 def fah_to_cel(temp_f):
   temp_lst = temp_f.split(deg_str)
-  c_val = float(temp_lst[0])
-  fahr  = c_val - 32 * 5/9
-  return f"{fahr:.1f}{deg_str}C"
+  f_val = float(temp_lst[0])
+  cels  = f_val - 32 * 5/9
+  return f"{cels:.1f}{deg_str}C"
 
 def temp_to_kel(temp):
     temp_lst = temp.split(deg_str)
