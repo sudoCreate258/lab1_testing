89a2956e84e5f3b5176644047b05b91e70c471ec
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-03 09:13:39 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,22 @@ from temp_func import print_steps, deg_str, cel_to_fah, fah_to_cel, temp_to_kel
 # DO NOT UPDATE CODE ABOVE THIS LINE
 
 # - cel_to_fah
-# TODO - (Input) Assign data for test cases - step 1
-# TODO - (Process) Introduce the assert statememts - step 3
-# TODO - (Output) Print passing cases - step 6
+# TODO - (Input) Assign data for test cases
+arg = f"{32}{deg_str}F"
+exp = f"{0}{deg_str}C"
+act = fah_to_cel(arg)
+
+# TODO - (Process) Introduce the assert statememts
+assert act == exp, f"{print_steps(arg)} {act} != {exp}, see temp_func.py"
+
+# TODO - (Output) Print passing cases
 
 # - fah_to_cel
-# TODO - (Input) Assign data for test cases - step 1
-# TODO - (Process) Introduce the assert statememts - step 3
-# TODO - (Output) Print passing cases - step 6
+# TODO - (Input) Assign data for test cases
+# TODO - (Process) Introduce the assert statememts
+# TODO - (Output) Print passing cases
 
 # - temp_to_kel
-# TODO - (Input) Assign data for test cases - step 1
-# TODO - (Process) Introduce the assert statememts - step 3
-# TODO - (Output) Print passing cases - step 6
+# TODO - (Input) Assign data for test cases
+# TODO - (Process) Introduce the assert statememts
+# TODO - (Output) Print passing cases
