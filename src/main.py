4310c6c5a7f264522a70f6b9c000edb44f9abc35
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-03-03 09:11:42 -0500

    Reorder import statements in main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
-from temp_func import deg_str, print_steps, cel_to_fah, fah_to_cel, temp_to_kel
+from temp_func import print_steps, deg_str, cel_to_fah, fah_to_cel, temp_to_kel
 # DO NOT UPDATE CODE ABOVE THIS LINE
 
 # - cel_to_fah
 # TODO - (Input) Assign data for test cases - step 1
-arg, exp, act = f"{0 }{deg_str}C", f"{32}{deg_str}F", cel_to_fah(arg)
-
 # TODO - (Process) Introduce the assert statememts - step 3
-assert act == exp, f"{print_steps(arg)} {act} != {exp} ? review temp_func.py"
-
 # TODO - (Output) Print passing cases - step 6
-opt = "cel_to_fah:\n\t"
-opt += f"{arg}{deg_str}C is {exp}{deg_str}F"
-print(opt)
 
 # - fah_to_cel
 # TODO - (Input) Assign data for test cases - step 1
